cb1219dccff92bb86dd63117ec9e53a4dd9c0450
Dato <examle@gmail.com>
2022-01-24 15:27:59 +0300

    loopEndLoop

--- js/script.js
+++ js/script.js
@@ -7,12 +7,29 @@ const personalMovieDB = {
       genres: [],
       privat: false
 };
-const a = prompt("Один из последних просмотренных фильмов?", " "),
-      b = prompt("На сколько очените его?", " "),
-      c = prompt("Один из последних просмотренных фильмов?", " "),
-      d = prompt("На сколько очените его?", " ");
 
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+      const a = prompt("Один из последних просмотренных фильмов?", " "),
+            b = prompt("На сколько оцените его?", " ");
 
-      console.log(personalMovieDB);
+      if(a != null && b != null && a != "" && b != "" && a.length < 50){
+            console.log("done");
+      } else {
+            console.log("err");
+            i--;
+      }
+
+personalMovieDB.movies[a] = b;
+}
+
+if (personalMovieDB.count < 10) {
+      console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+      console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+      console.log("Вы киноман");
+} else {
+      console.log("Произошла ошибка");
+}
+      
+console.log(personalMovieDB);
